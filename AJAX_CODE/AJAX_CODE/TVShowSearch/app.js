234844ada11a5198bc9e8a8e193237e3f8a7ba4e
Mohd Raza <razamohd6395@gmail.com>
2025-02-08 22:06:46 +0530

    update tv show app

--- AJAX_CODE/AJAX_CODE/TVShowSearch/app.js
+++ AJAX_CODE/AJAX_CODE/TVShowSearch/app.js
@@ -1,19 +1,26 @@
-const form = document.querySelector('#searchForm');
-form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const searchTerm = form.elements.query.value;
-    const config = { params: { q: searchTerm } }
-    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
-    makeImages(res.data)
-    form.elements.query.value = '';
-})
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  removeImages();
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+
+  makeImages(res.data);
+  form.elements.query.value = "";
+});
 
 const makeImages = (shows) => {
-    for (let result of shows) {
-        if (result.show.image) {
-            const img = document.createElement('IMG');
-            img.src = result.show.image.medium;
-            document.body.append(img)
-        }
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
     }
+  }
+};
+
+function removeImages() {
+  const images = document.querySelectorAll("img");
+  images.forEach((img) => img.remove());
 }
